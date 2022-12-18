c49805cc1e1484dbda4c68e8bef7f425dfbb61eb
wksaurey <70457200+wksaurey@users.noreply.github.com>
2022-12-17 18:49:31 -0700

    completed the first challenge for the third day

--- day3/find_total.py
+++ day3/find_total.py
@@ -1,8 +1,8 @@
 def find_priority(letter):
     if letter.isupper():
-        ASCII_CONVERSION = 96
+        ASCII_CONVERSION = 38
     if letter.islower():
-        ASCII_CONVERSION = 64
+        ASCII_CONVERSION = 96
 
     return ord(letter) - ASCII_CONVERSION
 
@@ -12,17 +12,22 @@ file = open(filename)
 
 total = 0
 for line in file:
+    line = line.strip()
     string_one = line[:int(len(line)/2)]
     string_two = line[int(len(line)/2):]
+    print()
 
-    print(line)
-    print(string_one)
-    print(string_two)
+    print(f'The complete line : {line}')
+    print(f'First half        : {string_one}')
+    print(f'Second half       : {string_two}')
     
     for letter in string_one:
         if letter in string_two:
+            print(f'Found match : {letter}')
+            print(f'Match Score : {find_priority(letter)}')
             total = total + find_priority(letter)
-            continue
+            print(f'Total Score : {total}')
+            break
 
 
 print(total)
