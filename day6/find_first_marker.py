abe47986177d68e2cfb01ca17b8eb65baa814648
Javier Hernandez <JHernandez@dw.local>
2022-12-22 11:54:07 -0700

    completed day6

--- day6/find_first_marker.py
+++ day6/find_first_marker.py
@@ -1,6 +1,16 @@
+import sys
+
+def duplicates(section):
+    for x in range(len(section)):
+        if section[x] in section[:x]:
+            return True
+    return False
+
 file = open('input.txt')
+UNIQUE_LEN = 14
 for line in file:
     line.strip()
-    for x in range(len(line-3)):
-        if !duplicates(line[x:x+4]:
-            print(x+4)
+    for x in range(len(line)-UNIQUE_LEN -1):
+        if not duplicates(line[x:x+UNIQUE_LEN]):
+            print(x+UNIQUE_LEN)
+            sys.exit(0)
